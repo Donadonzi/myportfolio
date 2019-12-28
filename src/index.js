3e43467a4b140975337a02b46becd6a144d70aee
Donadonzi <donadonzi@gmail.com>
2019-12-28 12:25:19 -0500

    Changing the API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './css/style.css';
 import axios from 'axios';
 
 
-
+/*
 window.navigator.geolocation.getCurrentPosition(res => {
 
         var lat = String(res.coords.latitude);
@@ -13,7 +13,6 @@ window.navigator.geolocation.getCurrentPosition(res => {
               
  });
     
-
 const getCity = async(lat, lon) => {
         const result = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?q=${lat},${lon}`, {
         params: {
@@ -29,7 +28,6 @@ const getCity = async(lat, lon) => {
     // return result.data.ParentCity.LocalizedName;
 };
 
-
 const getWeather = async(key) => {
     const response = await axios.get(`
     http://dataservice.accuweather.com/currentconditions/v1/${key}`
@@ -40,9 +38,23 @@ const getWeather = async(key) => {
     });
 
     console.log(response);
-    // console.log(`It's currently ${response.data[0].WeatherText}`);
+    console.log(`It's currently ${response.data[0].WeatherText}`);
 };
 
- 
+*/
+var apiKey = 'b0aa8c11a2cbe1989f85e8149167e9a1';
 
+window.navigator.geolocation.getCurrentPosition(res => {
+    const lat = String(res.coords.latitude);
+    const lon = String(res.coords.longitude);
+    getWeather(lat, lon, apiKey);   
+          
+});
 
+const getWeather = async(lat, lon, apiKey) => {
+    const response = await axios(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+   
+    console.log(response);
+    console.log(` ${response.data.weather[0].description} in ${response.data.name}. 
+    It's currently ${response.data.main.temp} degrees`);
+};
