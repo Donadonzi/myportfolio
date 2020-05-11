e8dee465d826c9a7beb33709f2f32360b3b1b7a3
Donadonzi <donadonzi@gmail.com>
2020-05-11 19:02:49 -0400

    modified portfolio section

--- src/index.js
+++ src/index.js
@@ -27,16 +27,16 @@ ReactDOM.render(
 // 	</div>,
 // 	document.getElementById('weather')
 // 	);
-	
-	
 
 
+
+/*
 // =============== JQUERY CODE FOR THUMBNAILS =================//
-(function($){ 
-		
+(function($){
+
 	// Main code
 	$(document).ready(function(){
-	
+
 		// Initialize the object on dom load
 		var navigator = new Navigator({
 			carousel: '#carousel',
@@ -45,11 +45,11 @@ ReactDOM.render(
 			shuffle: false,
 			chunkSize: window.matchMedia('(max-width: 600px)').matches ? 1 : 3
 		});
-		
-		navigator.init();		
+
+		navigator.init();
 	});
-	
-	
+
+
 	// A Navigator "class" responsible for navigating through the carousel.
 	function Navigator(config) {
 
@@ -59,23 +59,23 @@ ReactDOM.render(
 		this.chunkSize = config.chunkSize || 3; //how many items to show at a time (maximum)
 		// this.chunkSize = 1;
 		this.shuffle = config.shuffle || false; //should the list be shuffled first? Default is false.
-		
+
 		//private variables
 		this._items = $(config.carousel + ' li'); //all the items in the carousel
 		this._texts = $(config.carousel + ' p');
-		this._chunks = []; //the li elements will be split into chunks. 
+		this._chunks = []; //the li elements will be split into chunks.
 		this._textChunk = [];
 		this._visibleChunkIndex = 0; //identifies the index from the this._chunks array that is currently being shown
-		
+
 
 		this.init = function () {
-			
+
 			//remove visible tags
 			this._items.removeClass('visible');
 			this._texts.removeClass('visible');
 
 			//Shuffle the array if neccessary
-			if (this.shuffle) {					
+			if (this.shuffle) {
 				//shuffle list
 				this._items.sort(function() { return 0.5 - Math.random() });
 			}
@@ -83,32 +83,32 @@ ReactDOM.render(
 			//add visible class to first "chunkSize" items
 			this._items.slice(0, this.chunkSize).addClass('visible');
 			this._texts.slice(0, this.chunkSize).addClass('visible');
-			
+
 			//split array of items into chunks
 			this._chunks = this._splitItems(this._items, this.chunkSize);
 			this._textChunk = this._splitItems(this._texts, this.chunkSize);
-			
+
 
 			var self = this;
-			
+
 			//Set up the event handlers for previous and next button click
 			self.nextButton.on('click', function(e) {
 				self.handleNextClick(e);
 			}).show();
-			
+
 			self.prevButton.on('click', function(e) {
 				self.handlePrevClick(e);
 			});
-			
+
 			// Showing the carousel on load
 			self.carousel.addClass('active');
 		}
-		
+
 		//handle all code when previous button is clicked
 	  this.handlePrevClick = function (e) {
-			
+
 			e.preventDefault();
-	
+
 			//as long as there are some items before the current visible ones, show the previous ones
 			if (this._chunks[this._visibleChunkIndex - 1] !== undefined) {
 				this.showPrevItems();
@@ -118,63 +118,63 @@ ReactDOM.render(
 				this.showPrevItems();
 			}
 
-			
+
 		};
-		
+
 		//handle all code when next button is clicked
 		this.handleNextClick = function(e) {
-			
+
 			e.preventDefault();
-			
+
 			//as long as there are some items after the current visible ones, show the next ones
 			if (this._textChunk[this._visibleChunkIndex + 1] !== undefined) {
 				this.showNextItems();
 			}
 		};
-		
+
 		//show the next 3 items
 		this.showNextItems = function() {
-			
+
 			//remove visible class from current visible chunk
 			$(this._chunks[this._visibleChunkIndex]).removeClass('visible');
 			$(this._textChunk[this._visibleChunkIndex]).removeClass('visible');
-			
+
 			//add visible class to the next chunk
 			$(this._chunks[this._visibleChunkIndex + 1]).addClass('visible');
 			$(this._textChunk[this._visibleChunkIndex + 1]).addClass('visible');
-			
+
 			//update the current visible chunk
 			this._visibleChunkIndex++;
-			
+
 			//see if the end of the list has been reached.
 			this._checkForEnd();
-			
+
 		};
-		
+
 		//show the previous 3 items
 		this.showPrevItems = function() {
-			
+
 			//remove visible class from current visible chunk
 			$(this._chunks[this._visibleChunkIndex]).removeClass('visible');
 			$(this._textChunk[this._visibleChunkIndex]).removeClass('visible');
-			
+
 			//add visible class to the previous chunk
 			$(this._chunks[this._visibleChunkIndex - 1]).addClass('visible');
 			$(this._textChunk[this._visibleChunkIndex - 1]).addClass('visible');
-			
+
 			//update the current visible chunk
 			this._visibleChunkIndex--;
-			
+
 			//see if the beginning of the carousel has been reached.
 			this._checkForBeginning();
-			
+
 		};
-		
-		
+
+
 		//Determine if the previous button should be shown or not.
 		this._checkForBeginning = function() {
 			this.nextButton.show(); //the prev button was clicked, so the next button can show.
-			
+
 			if (this._chunks[this._visibleChunkIndex - 1] === undefined) {
 				this.prevButton.hide();
 			}
@@ -189,11 +189,11 @@ ReactDOM.render(
 				this.prevButton.show();
 			}
 		};
-		
+
 		//Determine if the next button should be shown or not.
 		this._checkForEnd = function() {
 			this.prevButton.show(); //the next button was clicked, so the previous button can show.
-			
+
 			if (this._chunks[this._visibleChunkIndex + 1] === undefined) {
 				this.nextButton.hide();
 			}
@@ -208,24 +208,24 @@ ReactDOM.render(
 				this.nextButton.show();
 			}
 		};
-		
-		
+
+
 		//This function takes an array "items" and splits it into subArrays each with a maximum length of "chunk".
 		this._splitItems = function(items, chunk) {
-			
+
 			var splitItems = [],
 			i = 0;
-			
+
 			while (items.length > 0) {
 				splitItems[i] = items.splice(0, chunk);
 				i++;
 			}
-			
+
 			return splitItems;
-		
+
 		};
 
-		
+
 
 
 
@@ -243,15 +243,15 @@ document.querySelector('.projects__list').addEventListener('mouseover', changeBG
 
 function changeBG(event) {
 	const item = event.target.closest('.projects__list__item');
-	
+
 	if(item) {
 		const className = item.dataset.num;
 		item.parentNode.parentNode.classList.add(`${className}`);
 		document.querySelector('.projects__gallery__helper').style.opacity = 1;
 		document.querySelectorAll('.projects__list__item').forEach(el => el.classList.add('shrink'));
 		document.getElementById(`${className}`).classList.add('focused');
-		
-	}		
+
+	}
 }
 
 const list = document.querySelectorAll('.projects__list__item');
@@ -263,11 +263,10 @@ function removeBG(event) {
 	event.target.parentNode.parentNode.classList.remove(`${className}`);
 	document.getElementById(`${className}`).classList.remove('focused');
 	document.querySelectorAll('.projects__list__item').forEach(el => el.classList.remove('shrink'));
-		
+
 }
 
-  
-    
 
 
- 
+*/
+
