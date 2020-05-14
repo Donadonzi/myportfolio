457f728aa9ba9c2efee191b34c399efd2bc71593
Donadonzi <donadonzi@gmail.com>
2020-05-14 09:26:44 -0400

    modified portfolio height again :|

--- src/components/VideoSelector.js
+++ src/components/VideoSelector.js
@@ -8,64 +8,64 @@ import ClearNight from '../videos/ClearNight.mp4';
 import RainyNight from '../videos/RainyNight-s.mp4';
 import SnowyNight from '../videos/SnowyNight.mp4';
 
-const DayOrNight= () => {
-    const hours = new Date().getHours();
-    let time = '';
-    hours >= 6 && hours <= 19 ? time = 'day' : time = 'night';
-    return time;
+const DayOrNight = () => {
+	const hours = new Date().getHours();
+	let time = '';
+	hours >= 6 && hours <= 20 ? time = 'day' : time = 'night';
+	return time;
 
 };
 
- const VideoSelector = (props) => {
-    
-    console.log(`VideoSelector update with code=${props.code}`);
-    const time = DayOrNight();
+const VideoSelector = (props) => {
 
-    let videoUrl = null;
+	console.log(`VideoSelector update with code=${props.code}`);
+	const time = DayOrNight();
 
-    if (time === 'day') {
-        if (props.code >= 200 && props.code <= 532) {
-            videoUrl = RainyDay;
-            document.getElementById('weather').className = '';
-            document.getElementById('weather').classList.add(`weather--RainyDay`);
-        } else if (props.code >= 600 && props.code <= 785) {
-            videoUrl = SnowyDay;
-            document.getElementById('weather').className = '';
-            document.getElementById('weather').classList.add(`weather--SnowyDay`);
-        } else if (props.code === 803 || props.code === 804) {
-            videoUrl = GreySky;
-            document.getElementById('weather').className = '';
-            document.getElementById('weather').classList.add(`weather--GreySky`);
-        }else {
-            videoUrl = ClearDay;
-            document.getElementById('weather').className = '';
-            document.getElementById('weather').classList.add(`weather--ClearDay`);
-            
-        }
-        
-    } else if (time === 'night') { 
-        if (props.code >= 200 && props.code <= 532) {
-            videoUrl = RainyNight;
-            document.getElementById('weather').className = '';
-            document.getElementById('weather').classList.add(`weather--RainyNight`);
-        } else if (props.code >= 600 && props.code <= 785) {
-            videoUrl = SnowyNight;
-            document.getElementById('weather').className = '';
-            document.getElementById('weather').classList.add(`weather--SnowyNight`);
-        } else {
-            videoUrl = ClearNight;
-            document.getElementById('weather').className = '';
-            document.getElementById('weather').classList.add(`weather--ClearNight`);
-        }
-    }
+	let videoUrl = null;
 
-    return (
-        <div className="bg-video">
-            <video key={videoUrl} className="bg-video__content" autoPlay muted loop>
-                <source src={videoUrl} type="video/mp4" />     
-            </video>
-        </div>
-    );
+	if (time === 'day') {
+		if (props.code >= 200 && props.code <= 532) {
+			videoUrl = RainyDay;
+			document.getElementById('weather').className = '';
+			document.getElementById('weather').classList.add(`weather--RainyDay`);
+		} else if (props.code >= 600 && props.code <= 785) {
+			videoUrl = SnowyDay;
+			document.getElementById('weather').className = '';
+			document.getElementById('weather').classList.add(`weather--SnowyDay`);
+		} else if (props.code === 803 || props.code === 804) {
+			videoUrl = GreySky;
+			document.getElementById('weather').className = '';
+			document.getElementById('weather').classList.add(`weather--GreySky`);
+		} else {
+			videoUrl = ClearDay;
+			document.getElementById('weather').className = '';
+			document.getElementById('weather').classList.add(`weather--ClearDay`);
+
+		}
+
+	} else if (time === 'night') {
+		if (props.code >= 200 && props.code <= 532) {
+			videoUrl = RainyNight;
+			document.getElementById('weather').className = '';
+			document.getElementById('weather').classList.add(`weather--RainyNight`);
+		} else if (props.code >= 600 && props.code <= 785) {
+			videoUrl = SnowyNight;
+			document.getElementById('weather').className = '';
+			document.getElementById('weather').classList.add(`weather--SnowyNight`);
+		} else {
+			videoUrl = ClearNight;
+			document.getElementById('weather').className = '';
+			document.getElementById('weather').classList.add(`weather--ClearNight`);
+		}
+	}
+
+	return (
+		<div className="bg-video">
+			<video key={videoUrl} className="bg-video__content" autoPlay muted loop>
+				<source src={videoUrl} type="video/mp4" />
+			</video>
+		</div>
+	);
 };
 
 export default VideoSelector;
