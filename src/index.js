6000464a681f3b288d3533b56c3e119ca315347b
Donadonzi <donadonzi@gmail.com>
2020-08-10 11:15:54 -0400

    removed aux section

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/style.css';
-import Current from './components/Current';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/style.css";
+import Current from "./components/Current";
 // import SimpleMenu from './components/Language';
 // import Loader from './components/Loader';
 // import  usePromiseTracker  from 'react-promise-tracker';
 
-
-ReactDOM.render(
-	<Current />,
-	document.getElementById('weather')
-);
+// ReactDOM.render(
+// 	<Current />,
+// 	document.getElementById('weather')
+// );
 
 /* --- Language menu ---
 ReactDOM.render(
@@ -19,12 +18,10 @@ ReactDOM.render(
 );
 */
 
-
 // ReactDOM.render(
 // 	<div>
 // 		<Loader />
-// 		<Current />	
+// 		<Current />
 // 	</div>,
 // 	document.getElementById('weather')
 // 	);
-
