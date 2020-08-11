a5196c39f8fd251ec0f656b70b9ffc6c596a5995
Donadonzi <donadonzi@gmail.com>
2020-08-11 13:02:01 -0400

    build a model for get weather

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/style.css";
 import Current from "./components/Current";
+
 // import SimpleMenu from './components/Language';
 // import Loader from './components/Loader';
 // import  usePromiseTracker  from 'react-promise-tracker';
 
-// ReactDOM.render(
-// 	<Current />,
-// 	document.getElementById('weather')
-// );
+
 
 /* --- Language menu ---
 ReactDOM.render(
@@ -25,3 +23,34 @@ ReactDOM.render(
 // 	</div>,
 // 	document.getElementById('weather')
 // 	);
+
+
+// Get the modal
+var modal = document.getElementById("weather-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("weather-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  ReactDOM.render(
+    <Current />,
+    document.getElementById('weather')
+  );
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
